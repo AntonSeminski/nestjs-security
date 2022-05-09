d217a79eedc614271972f0e43bf7a8d3b4719788
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2022-05-09 20:12:46 +0300

    v. 1.0.1 - invite token service

--- src/services/jwt-token/index.ts
+++ src/services/jwt-token/index.ts
@@ -1,3 +1,4 @@
 export * from './jwt-token.module'
 export * from './user-token.service'
 export * from './workspace-token.service'
+export * from './invite-token.service'
--- src/services/jwt-token/jwt-token.module.ts
+++ src/services/jwt-token/jwt-token.module.ts
@@ -3,11 +3,12 @@ import {WorkspaceTokenService} from './workspace-token.service';
 import {UserTokenService} from "./user-token.service";
 import {JwtModule} from '@nestjs/jwt';
 import {ConfigModule} from "@nestjs/config";
+import {InviteTokenService} from "./invite-token.service";
 
 @Module({
     imports: [JwtModule.register({secret: 'TEST_SECRET'}), ConfigModule],
-    providers: [UserTokenService, WorkspaceTokenService],
-    exports: [UserTokenService, WorkspaceTokenService]
+    providers: [UserTokenService, WorkspaceTokenService, InviteTokenService],
+    exports: [UserTokenService, WorkspaceTokenService, InviteTokenService]
 })
 export class JwtTokenModule {
 }
