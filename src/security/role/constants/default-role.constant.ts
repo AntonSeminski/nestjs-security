90bed4c97daab4b774846c86962d7624bc297144
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2022-05-10 22:15:00 +0300

    v. 1.0.19 - DEFAULT_ROLES - object view

--- src/security/role/constants/default-role.constant.ts
+++ src/security/role/constants/default-role.constant.ts
@@ -1,12 +1,12 @@
-export const DEFAULT_ROLES = [
-    {
+export const DEFAULT_ROLES = {
+    GUEST: {
         name: 'Guest',
         level: -1
     },
-    {
+    CEO: {
         name: 'CEO',
         level: 0
     }
-]
+}
 
 export const GUEST_ROLE_NAME = 'Guest';
