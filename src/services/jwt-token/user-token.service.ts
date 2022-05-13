cc6b801e3748768af7708c04135a38a08c708ea6
Anton Seminski <57789204+SeminGit@users.noreply.github.com>
2022-05-13 20:49:48 +0300

    v. 1.0.29 - token types

--- src/services/jwt-token/user-token.service.ts
+++ src/services/jwt-token/user-token.service.ts
@@ -1,13 +1,15 @@
-import { JwtTokenService } from "@asemin/nestjs-utils";
+import {JwtTokenService} from "@asemin/nestjs-utils";
 import {Injectable} from "@nestjs/common";
 import {ConfigService} from "@nestjs/config";
+import {ETokenType} from "@jira-killer/constants";
 
 @Injectable()
 export class UserTokenService extends JwtTokenService{
     constructor(private configService: ConfigService) {
         super(
             configService.get('JWT_USER_TOKEN_SECRET'),
-            configService.get('JWT_USER_TOKEN_EXPIRESIN')
+            configService.get('JWT_USER_TOKEN_EXPIRESIN'),
+            ETokenType.User
         );
     }
 }
--- src/services/jwt-token/workspace-token.service.ts
+++ src/services/jwt-token/workspace-token.service.ts
@@ -1,13 +1,15 @@
 import {Injectable} from '@nestjs/common';
 import {ConfigService} from "@nestjs/config";
 import {JwtTokenService} from "@asemin/nestjs-utils";
+import {ETokenType} from "@jira-killer/constants";
 
 @Injectable()
 export class WorkspaceTokenService extends JwtTokenService {
     constructor(private configService: ConfigService) {
         super(
             configService ? configService.get('JWT_WORKSPACE_TOKEN_SECRET') : process.env.JWT_WORKSPACE_TOKEN_SECRET,
-            configService ? configService.get('JWT_WORKSPACE_TOKEN_EXPIRESIN') : process.env.JWT_WORKSPACE_TOKEN_EXPIRESIN
+            configService ? configService.get('JWT_WORKSPACE_TOKEN_EXPIRESIN') : process.env.JWT_WORKSPACE_TOKEN_EXPIRESIN,
+            ETokenType.Workspace
         );
     }
 }
